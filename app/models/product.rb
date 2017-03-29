c470affce54eafc74c560e91be8c63e6ec445743
Type in your name <Type in your email>
2017-03-29 23:56:31 +0700

    get list product popular

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,20 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, uniqueness: :true
   accepts_nested_attributes_for :image_products, allow_destroy: true
+
+  def check_with_minsup?
+    a = Float(self.orders.size)
+    count_order = Float((Order.all.size))
+    ((a/count_order) > Settings.minsup)
+  end
+
+  def list_product_popular products
+    list_product = []
+    products.each do |t|
+      if t.check_with_minsup?
+        list_product.push(t.id)
+      end
+    end
+    list_product
+  end
 end
